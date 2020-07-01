b574ce4a8bc76cfd61891960779dc22b2938ffba
yjliu <1229931433@qq.com>
2020-07-01 14:42:38 +0800

    路由配置并设置标题属性

--- src/routes/index.js
+++ src/routes/index.js
@@ -7,14 +7,54 @@ import Home from '../views/Home'
 const Users = () => <h2>use页面</h2>;
 
 
+// A special wrapper for <Route> that knows how to
+// handle "sub"-routes by passing them in a `routes`
+// prop to the component it renders.
+function RouteWithSubRoutes(route) {
+  return (
+    <Route
+      path={route.path}
+      render={props => {
+        document.title = route.meta.title || "默认title";
+        return (
+          // pass the sub-routes down to keep nesting
+          <route.component {...props} extra routes={route.routes} />
+        )
+      }}
+    />
+  );
+}
+
+const routes = [
+  {
+    path: "/home",
+    component: Home,
+    meta: {
+      title: 'home页面'
+    }
+  },
+  {
+    path: "/users",
+    component: Users,
+    meta: {
+      title: 'use页面'
+    }
+  },
+  {
+    path: "*",
+    component: App,
+    meta: {
+      title: '首页'
+    }
+  },
+]
+
 export default (
   <Router>
     <Switch>
-      <Route path="/" exact component={App} />
-      <Route path="/home" component={Home} />
-      <Route path="/users" component={Users} />
+      {routes.map((route, i) => (
+        <RouteWithSubRoutes key={i} {...route} />
+      ))}
     </Switch>
   </Router>
 );
-
-
